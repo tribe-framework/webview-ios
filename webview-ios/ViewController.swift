b71cf94916c3ec6a63d19ccd6ab5d4338a4387d1
akshay / macmini <akshay.wildfire@pm.me>
2023-03-07 00:39:00 +0530

    our standard webview code

--- webview-ios/ViewController.swift
+++ webview-ios/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let webView: WKWebView = {
+    let prefs = WKWebpagePreferences()
+    prefs.allowsContentJavaScript = true
+    let configuration = WKWebViewConfiguration()
+    configuration.defaultWebpagePreferences = prefs
+    
+        let webView = WKWebView(frame: .zero,
+                                configuration: configuration)
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.init(red: 255/255, green: 51/255, blue: 153/255, alpha: 1)
+        view.addSubview(webView)
+        
+        guard let url = URL(string: "https://tribe-framework.org") else {
+            return
+        }
+        
+        webView.scrollView.isScrollEnabled = false
+        
+        //to prevent white flashing
+        webView.isOpaque = false
+        webView.backgroundColor = UIColor.init(red: 255/255, green: 51/255, blue: 153/255, alpha: 1)
+        
+        webView.load(URLRequest(url: url))
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
